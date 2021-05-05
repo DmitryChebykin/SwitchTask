fa2b51fbe82062cf45390a860ebd53fb3c5deae3
Dmitry Chebykin <dim_c@mail.ru>
2021-05-05 15:21:33 +0700

    fix

--- src/main/java/SwitchApp.java
+++ src/main/java/SwitchApp.java
@@ -1,134 +1,137 @@
 import java.util.Scanner;
 
+enum AllowedOperation {
+    ADDITION("Сложение", "+", 1),
+    SUBTRACTION("Вычитание", "-", 2),
+    MULTIPLICATION("Умножение", "*", 3),
+    DIVISION("Деление", ":", 4);
+
+    private final String ruOperationName;
+    private final String operationPrintSign;
+    private final int operationCode;
+
+    AllowedOperation(String operationName, String operationSign, int operationCode) {
+        this.ruOperationName = operationName;
+        this.operationPrintSign = operationSign;
+        this.operationCode = operationCode;
+    }
+
+    public String getRuOperationName() {
+        return ruOperationName;
+    }
+
+    public String getOperationPrintSign() {
+        return operationPrintSign;
+    }
+
+    public int getOperationCode() {
+        return operationCode;
+    }
+}
+
 public class SwitchApp {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static double number1;
+    private static double number2;
+    private static int codeOperation;
+
     public static void main(String[] args) {
         System.out.println("Над двумя вводимыми числами определены следующие операции:");
         printAllowedOperationsInfo();
 
-        System.out.println("Введите два числа и код операции через пробелы (например:  124  23 3):");
-        double[] operationData = getInputOperationData();
+        System.out.println("Потребуется ввести два числа и код операции.");
+        getOperationData();
 
-        printOperationInfo(operationData);
+        printOperationInfo();
     }
 
-    private static void printOperationInfo(double[] operationData) {
-        if (!isInputOperationAllowed(operationData[2])) {
+    private static void printOperationInfo() {
+        if (!isOperationAllowed(codeOperation)) {
             System.out.println("Действие невозможно, для заданного кода не определена операция.");
-        } else if (isDivisionByZero(operationData)) {
-            System.out.println("Действие невозможно. Делить на ноль нельзя.");
-        } else {
-            printInputOperationDescription(operationData);
-
-            double result = getResult(operationData);
-            printOperationResultPrompt(operationData, result);
+            return;
         }
+
+        if (isDivisionByZero()) {
+            System.out.println("Действие невозможно. Делить на ноль нельзя.");
+            return;
+        }
+
+        printInputOperationDescription();
+
+        double result = getResult(number1, number2);
+        printOperationResultPrompt(result);
     }
 
-    private static void printInputOperationDescription(double[] operationData) {
-        AllowedOperation operation = AllowedOperation.values()[(int) operationData[2] - 1];
+    private static void printInputOperationDescription() {
+        AllowedOperation operation = AllowedOperation.values()[codeOperation - 1];
 
         String operationType = operation.name();
         String ruOperationType = operation.getRuOperationName();
         System.out.println("Вы задали операцию " + operationType + "(" + ruOperationType + "):");
     }
 
-    private static void printOperationResultPrompt(double[] operationData, double result) {
-        AllowedOperation operation = AllowedOperation.values()[(int) operationData[2] - 1];
-        System.out.println(operationData[0] + " " +
-                operation.getOperationPrintSign() + " " +
-                operationData[1] + " = " + result);
+    private static void printOperationResultPrompt(double result) {
+        AllowedOperation operation = AllowedOperation.values()[codeOperation - 1];
+        System.out.println(number1 + " " + operation.getOperationPrintSign() + " " +
+                number2 + " = " + result);
     }
 
-    private static double getResult(double[] operationData) {
+    private static double getResult(double number1, double number2) {
         double operationResult = 0;
 
-        switch ((int) operationData[2]) {
+        switch (codeOperation) {
             case 1:
-                operationResult = operationData[0] + operationData[1];
+                operationResult = number1 + number2;
                 break;
             case 2:
-                operationResult = operationData[0] - operationData[1];
+                operationResult = number2 - number1;
                 break;
             case 3:
-                operationResult = operationData[0] * operationData[1];
+                operationResult = number1 * number2;
                 break;
             case 4:
-                operationResult = operationData[0] / operationData[1];
+                operationResult = number1 / number2;
                 break;
         }
 
         return operationResult;
     }
 
-    private static double[] getInputOperationData() {
-        Scanner scanner = new Scanner(System.in);
+    private static void getOperationData() {
 
-        double[] operationData = new double[3];
         boolean isRightInput = false;
 
         while (!isRightInput) {
-            String inputString = scanner.nextLine().replaceAll("\\s+", " ").trim();
-            String[] parsedString = inputString.split("\\s");
             try {
-                operationData[0] = Double.parseDouble(parsedString[0]);
-                operationData[1] = Double.parseDouble(parsedString[1]);
-                operationData[2] = Double.parseDouble(parsedString[2]);
+                System.out.println("Введите первое число:");
+                number1 = scanner.nextDouble();
 
-                if (operationData[2] % 1 != 0) {
-                    System.out.println("Неверный ввод кода операции, попробуйте снова!");
-                    continue;
-                }
+                System.out.println("Введите второе число:");
+                number2 = scanner.nextDouble();
+
+                System.out.println("Введите код операции:");
+                codeOperation = scanner.nextInt();
 
                 isRightInput = true;
             } catch (Exception e) {
                 System.out.println("Неверный ввод, попробуйте снова!");
+                scanner.nextLine();
             }
         }
-
-        return operationData;
     }
 
-    private static boolean isInputOperationAllowed(double operationCode) {
+    private static boolean isOperationAllowed(double operationCode) {
         int countAllowedOperations = AllowedOperation.values().length;
         return 1 <= (int) operationCode && (int) operationCode <= countAllowedOperations;
     }
 
-    private static boolean isDivisionByZero(double[] operationData) {
-        return operationData[1] == 0 && operationData[2] == 4;
+    private static boolean isDivisionByZero() {
+        return number2 == 0 && codeOperation == 4;
     }
 
     private static void printAllowedOperationsInfo() {
         for (AllowedOperation operation : AllowedOperation.values()) {
             System.out.printf("%-9s %s%n", operation.getRuOperationName(), " код " + operation.getOperationCode() + "");
-        }
-    }
-
-    public enum AllowedOperation {
-        ADDITION("Сложение", "+", 1),
-        SUBTRACTION("Вычитание", "-", 2),
-        MULTIPLICATION("Умножение", "*", 3),
-        DIVISION("Деление", ":", 4);
-
-        private final String ruOperationName;
-        private final String operationPrintSign;
-        private final int operationCode;
-
-        AllowedOperation(String operationName, String operationSign, int operationCode) {
-            this.ruOperationName = operationName;
-            this.operationPrintSign = operationSign;
-            this.operationCode = operationCode;
-        }
-
-        public String getRuOperationName() {
-            return ruOperationName;
-        }
-
-        public String getOperationPrintSign() {
-            return operationPrintSign;
-        }
-
-        public int getOperationCode() {
-            return operationCode;
         }
     }
 }
